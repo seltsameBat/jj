72e2940e8561e067390d7aee8dec947f48156129
seltsameBat <152424723+seltsameBat@users.noreply.github.com>
2025-02-08 21:55:01 +0300

    Update app.js

--- matching/js/app.js
+++ matching/js/app.js
@@ -5,99 +5,167 @@ const deck = document.querySelector('.deck');
 const ul = document.createDocumentFragment();
 const cardsList = [];
 
-// TODO: Use svg or png format to display icons according the file path
-const uri = document.baseURI;
-const uriContains = uri[0] + uri[1] + uri[2] + uri[3] + uri[4] + uri[5] + uri[6] + uri[7];
-try {
-    if (uriContains.toLowerCase() === 'file:///') throw true;
-	if (uriContains.toLowerCase() !== 'file:///') throw false;
-}
-catch(err) {
-	if (err) {
-        for(let i = 1; i <= 16; i++) {
-            const li = document.createElement('li');
-            let svg = '';
-            li.className = 'card';
-            if (i > 8) {
-                let j = i - 8;
-                svg = '<svg role="img" class="icon fallback fallback-png-'+ j +'" title="0' + j + '"></svg>';
-                li.innerHTML = svg;
-            } else {
-                svg = '<svg role="img" class="icon fallback fallback-png-'+ i +'" title="0' + i + '"></svg>';
-                li.innerHTML = svg;
-            }
-            cardsList.push(li);
-        }
-	} else if (!err) {
-        for(let i = 1; i <= 16; i++) {
-            const li = document.createElement('li');
-            let svg = '';
-            li.className = 'card';
-            if (i > 8) {
-                let j = i - 8;
-                svg = '<svg role="img" class="icon" title="0' + j + '"><use xlink:href="./img/sprites.svg#icon-' + j + '"></use></svg>';
-                li.innerHTML = svg;
-            } else {
-                svg = '<svg role="img" class="icon" title="0' + i + '"><use xlink:href="./img/sprites.svg#icon-' + i + '"></use></svg>';
-                li.innerHTML = svg;
-            }
-            cardsList.push(li);
-        }
-	}
-}
+// Use PNG images (02.png - 12.png)
+const imagePaths = [
+    "02.png", "03.png", "05.png", "06.png",
+    "08.png", "09.png", "10.png", "12.png"
+];
 
-const shuffledCards = shuffle(cardsList);
-for(let card of shuffledCards) {
-    ul.appendChild(card);
-}
+// Create duplicate cards for matching
+const cardImages = [...imagePaths, ...imagePaths];
 
+// Shuffle the cards
+const shuffledCards = shuffle(cardImages);
+
+shuffledCards.forEach((img) => {
+    const li = document.createElement('li');
+    li.className = 'card';
+    li.dataset.image = img;
+
+    const imgElement = document.createElement('img');
+    imgElement.src = `img/png/${img}`;
+    imgElement.alt = `Memory Card ${img}`;
+    imgElement.classList.add('hidden');
+
+    li.appendChild(imgElement);
+    cardsList.push(li);
+});
+
+// Append shuffled cards to the deck
+cardsList.forEach(card => ul.appendChild(card));
 deck.appendChild(ul);
 
-let cards = document.querySelectorAll('.card');
+let openCards = [];
+let matchedCards = [];
+let movesCounter = 0;
 const moves = document.querySelector('.moves');
 const restartBtn = document.querySelector('.restart');
 const min = document.querySelector('.min');
 const sec = document.querySelector('.sec');
-let openCards = [];
-let matchedCards = [];
-let movesCounter = 0;
-moves.textContent = movesCounter + ' Moves';
+const stars = document.querySelectorAll('.stars li');
+const comment = document.querySelector('.comment');
+const finalScoreModal = document.querySelector('.modal');
+const closeModal = document.querySelector('.close');
+const playAgain = document.querySelector('.play-again');
+const totalTime = document.querySelector('#total-time');
+let timerOn = false;
+let playerTime;
 
-// TODO: Reset game
-restartBtn.addEventListener('click', function() {
-    restart(deck, ul);
+// Initialize Moves
+moves.textContent = `${movesCounter} Moves`;
+
+// Restart Game
+restartBtn.addEventListener('click', restart);
+
+// Card click event listener
+deck.addEventListener('click', function (e) {
+    if (e.target.tagName === 'LI' && !e.target.classList.contains('match')) {
+        showCard(e.target);
+    }
 });
 
-let playerTime = 0;
-let timerOn = false;
-// TODO: Start running timer
+// Timer Start
 deck.addEventListener('click', function(e) {
+    if (!timerOn && e.target.classList.contains('card')) {
+        startTimer();
+    }
+});
+
+function showCard(card) {
+    if (openCards.length >= 2 || card.classList.contains('open')) return;
+
+    card.classList.add('open');
+    card.querySelector('img').style.display = 'block';
+    openCards.push(card);
+
+    if (openCards.length === 2) {
+        movesCounter++;
+        moves.textContent = `${movesCounter} Moves`;
+        updateStars();
+        checkMatch();
+    }
+}
+
+function checkMatch() {
+    const [firstCard, secondCard] = openCards;
+    
+    if (firstCard.dataset.image === secondCard.dataset.image) {
+        openCards.forEach(card => card.classList.add('match'));
+        matchedCards.push(...openCards);
+        openCards = [];
+
+        if (matchedCards.length === cardsList.length) {
+            setTimeout(showFinalScore, 500);
+        }
+    } else {
+        setTimeout(() => {
+            openCards.forEach(card => {
+                card.classList.remove('open');
+                card.querySelector('img').style.display = 'none';
+            });
+            openCards = [];
+        }, 800);
+    }
+}
+
+// Restart Game
+function restart() {
+    openCards = [];
+    matchedCards = [];
+    movesCounter = 0;
+    moves.textContent = "0 Moves";
+
+    // Reset all cards
+    cardsList.forEach(card => {
+        card.classList.remove('open', 'match');
+        card.querySelector('img').style.display = 'none';
+    });
+
+    // Shuffle and reassign cards
+    const shuffled = shuffle(cardImages);
+    cardsList.forEach((card, index) => {
+        card.dataset.image = shuffled[index];
+        card.querySelector('img').src = `img/png/${shuffled[index]}`;
+    });
+
+    // Append to deck
+    deck.innerHTML = "";
+    cardsList.forEach(card => deck.appendChild(card));
+
+    // Reset Timer & Stars
+    timerOn = false;
+    clearInterval(playerTime);
+    min.textContent = "00";
+    sec.textContent = "00";
+    stars.forEach(star => star.style.display = 'inline-block');
+    comment.textContent = "";
+}
+
+// Show Final Score Modal
+function showFinalScore() {
+    finalScoreModal.style.display = 'block';
+    totalTime.textContent = `${min.textContent}m ${sec.textContent}s`;
+    updateStars();
+}
+
+playAgain.addEventListener('click', function () {
+    restart();
+    closeModalHandler();
+});
+
+closeModal.addEventListener('click', closeModalHandler);
+
+function closeModalHandler() {
+    finalScoreModal.style.display = 'none';
+}
+
+// Timer Functionality
+function startTimer() {
+    timerOn = true;
     let minuteCounter = 0;
     let secondCounter = 0;
 
-    // TODO: Stop timer
-    if (matchedCards.length === 16) {
-        timerOn = false;
-        clearInterval(playerTime);
-        return;
-    }
-
-    // Do nothing if timer is running
-    if (timerOn) {
-        return;
-    }
-
-    // TODO: Start timer
-    if (e.target.classList.contains('card')) {
-        timerOn = true;
-        min.textContent = '00';
-        sec.textContent = '00';
-        minuteCounter = 0;
-        secondCounter = 0;
-        playerTime = setInterval(timer, 1000);
-    }
-
-    function timer() {
+    playerTime = setInterval(() => {
         if (secondCounter === 59) {
             secondCounter = 0;
             minuteCounter++;
@@ -105,171 +173,32 @@ deck.addEventListener('click', function(e) {
         }
         secondCounter++;
         sec.textContent = secondCounter < 10 ? '0' + secondCounter : secondCounter;
-    }
-});
-
-
-// TODO: Reveal cards
-for(let card of cards) {
-    card.addEventListener('click', show);
+    }, 1000);
 }
 
-function shuffle(arr) {     // Function to shuffle cards
-    let shuffled = [];
-    let currentLength = arr.length;
-    while(currentLength !== 0) {
-      let randomNumber = Math.floor(Math.random() * currentLength);
-      shuffled.push(arr[randomNumber]);
-      currentLength -= 1;
-      arr[randomNumber] = arr[currentLength];
+// Update Star Rating
+function updateStars() {
+    if (movesCounter > 18) {
+        stars[2].style.display = 'none';
+        comment.textContent = 'Memory Made of Stone?';
     }
-    return shuffled;
-}
-
-function show(e) {
-    //  Prevent opening more than two cards
-    if (openCards.length >= 2 || e.target.classList.contains('open', 'show') || e.target.classList.contains('match') || e.target.classList.contains('icon')) {
-        return;
-    }
-
-    // Push open cards to array
-    e.target.classList.add('open', 'show');
-    e.target.firstElementChild.classList.add('display-icon');
-    openCards.push(e.target);
-
-    // Close open cards after sometime
-    if (openCards.length === 2) {
-        movesCounter++;
-        moves.textContent = movesCounter === 1 ? 1 + ' Move' : movesCounter + ' Moves';
-        starScore('.stars', 'inline-block');
-        match();
-    }
-}
-
-function match() {
-    if (openCards[0].firstElementChild.getAttribute('title') === openCards[1].firstElementChild.getAttribute('title')) {
-        openCards.map(function(card) {
-            card.className = 'card match';
-            matchedCards.push(card);
-        });
-        setTimeout(finalScore, 500);
-        openCards = [];
-    } else {
-        // Hide cards if they do not match
-        setTimeout(function() {
-            for(let opencard of openCards) {
-                opencard.style.animation = 'shake 0.5s';
-                setTimeout(function() {
-                    opencard.style.animation = 'reverseflip 0.4s';
-                    opencard.firstElementChild.style.animation = 'hideicon 0.4s';
-                }, 500);
-            }
-            setTimeout(function() {
-                for(let opencard of openCards) {
-                    opencard.classList.remove('open', 'show');
-                    opencard.firstElementChild.classList.remove('display-icon');
-                    opencard.style.animation = '';
-                    opencard.firstElementChild.style.animation = '';
-                    opencard.removeAttribute('style');
-                    opencard.firstElementChild.removeAttribute('style');
-                }
-                openCards = [];
-            }, 800);
-        }, 900);
-    }
-}
-
-function finalScore() {
-    const modal = document.querySelector('.modal');
-    const closeModal = document.querySelector('.close');
-    const playAgain = document.querySelector('.play-again');
-    const minFinal = document.querySelector('.min');
-    const secFinal = document.querySelector('.sec');
-    const score = document.querySelector('#total-time');
-    const container = document.querySelector('.container');
-    const backdrop = document.querySelector('.backdrop');
-    const containerHeight = container.offsetHeight;
-
-    playAgain.addEventListener('click', function() {
-        restart(deck, ul);
-        close();
-    });
-
-    closeModal.addEventListener('click', close);
-
-    // TODO: Display modal
-    if (matchedCards.length === 16) {
-        modal.style.display = 'block';
-        backdrop.style.display = 'block';
-        score.textContent = minFinal.textContent + 'm ' + secFinal.textContent + 's';
-        backdrop.classList.add('backdrop-show');
-        backdrop.style.height = containerHeight + 'px';
-        starScore('.modal-stars', 'none', '#f5ce67');
-    }
-
-    function close() {
-        modal.style.display = 'none';
-        backdrop.style.display = 'none';
-    }
-}
-
-function restart(parentTag, fragment) {
-    // Shuffle cards
-    const cardsArr = [];
-    cards.forEach(function(card) {
-        cardsArr.push(card);
-    });
-    parentTag.innerHTML = '';
-    cards = shuffle(cardsArr);
-    for(let card of cards) {
-        fragment.appendChild(card);
-    }
-    parentTag.appendChild(fragment);
-
-    // Display closed cards
-    if (openCards.length === 0 && matchedCards.length === 0 && movesCounter === 0) {
-        return;
-    }
-
-    matchedCards.map(function(card) {
-        card.classList.remove('match');
-        card.firstElementChild.classList.remove('display-icon');
-    });
-
-    openCards.map(function(card) {
-        card.classList.remove('open', 'show');
-        card.firstElementChild.classList.remove('display-icon');
-    });
-
-    movesCounter = 0;
-    moves.textContent = movesCounter + ' Moves';
-    min.textContent = '00';
-    sec.textContent = '00';
-    openCards = [];
-    matchedCards = [];
-    starScore('.stars');
-    timerOn = false;
-    clearInterval(playerTime);
-}
-
-// Function to display the number of stars received according to player's moves
-function starScore(name, display, color) {
-    const stars = document.querySelector(name).children;
-    const comment = document.querySelector('.comment');
-    if (movesCounter > 18 && movesCounter < 25) {
-        stars[2].style.color = 'black';
-        stars[2].style.display = display;
-        comment.textContent = 'Genuis';
-    } else if (movesCounter <= 25) {
-        stars[1].style.color = 'black';
-        stars[1].style.display = display;
-        stars[2].style.display = display;
+    if (movesCounter > 25) {
+        stars[1].style.display = 'none';
         comment.textContent = 'Good Job!';
-    } else if (movesCounter >= 18) {
-        stars[1].style.display = 'inline-block';
-        stars[2].style.display = 'inline-block';
-        stars[2].style.color = color || 'orange';
-        stars[1].style.color = color || 'orange';
-        comment.textContent = 'MEMORY MADE OF STONE??!!!';
     }
+    if (movesCounter > 30) {
+        stars[0].style.display = 'none';
+        comment.textContent = 'Try Again!';
+    }
+}
+
+// Fisher-Yates Shuffle Algorithm
+function shuffle(arr) {
+    let currentIndex = arr.length, randomIndex;
+    while (currentIndex !== 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+        [arr[currentIndex], arr[randomIndex]] = [arr[randomIndex], arr[currentIndex]];
+    }
+    return arr;
 }
